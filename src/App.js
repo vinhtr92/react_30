ff614800809d190c67d891ecd46086eb616ea05e
vinh <tranvinh@hotmail.com.vn>
2022-04-11 17:43:15 +0700

    Assignment 1: Time to Practice: React & Component Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import Expenses from "./components/Expenses";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 452,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
+      <Expenses data={expenses} />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +40,31 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <ExpenseItem
+        id={expenses[0].id}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      /> */}
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from "./ExpenseItem";
+function Expenses(props) {
+  return props.data.map((item) => {
+    return (
+      <ExpenseItem
+        key={props.data.indexOf(item)}
+        id={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
+    );
+  });
+}
+
+export default Expenses;
