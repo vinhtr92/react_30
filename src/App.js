318b311c8d8b59ee40cb3b806b83610ead48bede
vinh <tranvinh@hotmail.com.vn>
2022-04-12 11:35:13 +0700

    upgrade react 18

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from "./logo.svg";
 import Expenses from "./components/Expenses";
+import React from "react";
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
-      amount: 94.12,
+      amount: 94.122,
       date: new Date(2020, 7, 14),
     },
     { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
-      amount: 294.67,
+      amount: 294.687,
       date: new Date(2021, 2, 28),
     },
     {
@@ -24,49 +23,62 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div className="App">
       <Expenses data={expenses} />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
     </div>
   );
+
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement(Expenses, { data: expenses })
+  // );
+
+  // return (
+  //   <div className="App">
+  //     <Expenses data={expenses} />
+  //     {/* <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header> */}
+  //     {/* <ExpenseItem
+  //       id={expenses[0].id}
+  //       title={expenses[0].title}
+  //       amount={expenses[0].amount}
+  //       date={expenses[0].date}
+  //     />
+  //     <ExpenseItem
+  //       id={expenses[1].id}
+  //       title={expenses[1].title}
+  //       amount={expenses[1].amount}
+  //       date={expenses[1].date}
+  //     />
+  //     <ExpenseItem
+  //       id={expenses[2].id}
+  //       title={expenses[2].title}
+  //       amount={expenses[2].amount}
+  //       date={expenses[2].date}
+  //     />
+  //     <ExpenseItem
+  //       id={expenses[3].id}
+  //       title={expenses[3].title}
+  //       amount={expenses[3].amount}
+  //       date={expenses[3].date}
+  //     /> */}
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(props) {
   return (
-    <div className="expense-item" id={props.id}>
+    <Card className="expense-item" id={props.id}>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
